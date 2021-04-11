bffd0a2fa366b6551b582d5307342f6e48d269b3
노아 <cpn1011@gmail.com>
2021-04-11 18:45:30 +0900

    feature_balanceGame_#7: Added

---
+++ src/contents/addCountTxt/addCountTxt.resolvers.ts
@@ -0,0 +1,34 @@
+import { Resolvers } from "../../types";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    addCountTxt: async (_, { id }, { client }) => {
+      if (!id) {
+        return {
+          ok: false,
+          error: "Id not found",
+        };
+      }
+
+      const rightAnswer = await client.answer.findUnique({
+        where: {
+          id,
+        },
+      });
+      console.log(rightAnswer);
+      await client.answer.update({
+        where: {
+          id,
+        },
+        data: {
+          winCount: rightAnswer.winCount + 1,
+        },
+      });
+      return {
+        ok: true,
+      };
+    },
+  },
+};
+
+export default resolvers;
